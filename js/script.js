fea83adbd6e8e9b09b55b59c4e2956dd4231cd54
Vitor Matos <97922568+ViktorM4@users.noreply.github.com>
2024-10-20 08:50:34 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const nome = document.getElementById("nome");
+const peso = document.getElementById("peso");
+const altura = document.getElementById("altura");
+
+
+function calcularImc() {
+    let imc = peso.value / (altura.value * altura.value);
+    let mensagem = nome.value+", seu IMC é: "+imc.toFixed(3);
+    alert(mensagem);
+}
